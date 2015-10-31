dcbaa60416b547272d59f3edfb448619d3ae5b69
wanglei <398758695@qq.com>
2015-10-31 20:45:06 +0800

    delete something

--- lab2/handle.h
+++
@@ -1,8 +0,0 @@
-#ifndef HANDLE_H
-#define HANDLE_H
-
-#include "http-tree.h"
-
-void Handle_main (int fd, Http_t tree);
-
-#endif
--- lab2/parse.h
+++
@@ -1,8 +0,0 @@
-#ifndef PARSE_H
-#define PARSE_H
-
-#include "http-tree.h"
-
-Http_t Parse_parse(int fd);
-
-#endif
